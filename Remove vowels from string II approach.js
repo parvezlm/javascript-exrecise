e2f8badd8b8336829f14232a68c79de5a51bb818
parvezlm <parvezstack@gmail.com>
2023-03-16 13:26:59 +0530

    Remove vowels from string

---
+++ Remove vowels from string II approach.js
@@ -0,0 +1,9 @@
+// Remove vowels from string
+function removeVowels(str) {
+    let newStr = '';
+    return str.replace(/[aeiou]/g,'');
+  }
+  
+  let res = removeVowels('what is your name ?');
+  console.log(res);
+  //output : wht s yr nm ?
