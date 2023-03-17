3112d200cea524a830e486bd97580e5c69f2e03b
parvezlm <parvezstack@gmail.com>
2023-03-17 11:04:56 +0530

    Count type of Characters

---
+++ Count type of Characters.js
@@ -0,0 +1,28 @@
+// Count type of Characters
+function count(str) {
+    let upper = 0;
+    let lower = 0;
+    let spe = 0;
+    let digit = 0;
+    const hasNumber = /\d/;
+    const isLowerCaseLetter = /[a-z]/;
+    const isUpperCaseLetter = /[A-Z]/;
+    
+    for (let i = 0; i < str.length; i++) {
+        let ch = str[i];
+        if (hasNumber.test(ch)) {
+            digit++;
+        } else if (isUpperCaseLetter.test(ch)) {
+           upper++;
+        } else if (isLowerCaseLetter.test(ch)) {
+           lower++;
+        } else {
+           spe++;
+        }
+    }
+    return [upper, lower, digit, spe]
+}
+
+let res = count('*GeEkS4GeEkS*');
+console.log(res);
+//output: 6,4,1,2;
