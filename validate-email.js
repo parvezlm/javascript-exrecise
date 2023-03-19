203a2ddb13c6f4bd9f9d283e45ab9422c57e2b44
parvezlm <parvezstack@gmail.com>
2023-03-19 12:56:02 +0530

    validate email

---
+++ validate-email.js
@@ -0,0 +1,22 @@
+// validate email
+function checkmail(val) {
+    let regex = /^\S+@\S+\.\S+$/;
+    let elm = document.getElementById('error');
+    let errMsg = {
+        required: 'mail is required',
+        pattern: 'mail is not valid'
+    };
+    try {
+        if (!val) {
+            elm.innerText = errMsg.required;
+            elm.classList.add('invalid');
+        } else if (!regex.test(val)) {
+            elm.innerText = errMsg.pattern;
+            elm.classList.add('invalid');
+        } else {
+            elm.classList.remove('invalid');
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
