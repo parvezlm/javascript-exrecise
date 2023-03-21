e7deefb3a83d1271e3a1284610eaf87d8613668c
parvezlm <parvezstack@gmail.com>
2023-03-21 17:16:09 +0530

    First and last occurrences of x usnig binary search

---
+++ First and last occurrences of x.js
@@ -0,0 +1,46 @@
+// First and last occurrences of x usnig binary search
+
+function search(arr, num) {
+   return [firstOccurrence(arr, num), lastOccurrence(arr, num)];
+}
+
+function firstOccurrence(arr, num) {
+    let start = 0;
+    let end = arr.length - 1;
+    let ans;
+    while (start <= end) {
+        let mid = Math.floor(start + (end - start) / 2);
+        if (num === arr[mid]) {
+            ans = mid;
+            end = mid - 1;
+        } else if (num > arr[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    
+    return ans;
+}
+
+function lastOccurrence(arr, num) {
+    let start = 0;
+    let end = arr.length - 1;
+    let ans;
+    while (start <= end) {
+        let mid = Math.floor(start + (end - start) / 2);
+        if (num === arr[mid]) {
+            ans = mid;
+            start = mid + 1;
+        } else if (num > arr[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return ans;
+} 
+
+
+let res = search([ 1, 3, 5, 5, 5, 5, 67, 123, 125], 5);
+console.log(res);
