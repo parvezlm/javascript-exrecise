1b7be8ad2c3039f5294b3450aeb2fa6beed2db5d
parvezlm <parvezstack@gmail.com>
2023-03-27 12:03:00 +0530

    Remove common characters and concatenate

---
+++ Remove common characters and concatenate.js
@@ -0,0 +1,22 @@
+// Remove common characters and concatenate
+
+function concatenatedString(s1, s2) {
+    let elm1 = removeDup(s1, s2);
+    let elm2 = removeDup(s2, s1);
+    return elm1 + elm2;
+}
+
+function removeDup(str1, str2) {
+  let elm = '';
+  for (let i = 0; i < str2.length; i++) {
+    if (str1.includes(str2[i])) {
+      let regExp = new RegExp(str2[i], 'g');
+      elm = str1.replace(regExp, '');
+      str1 = elm;
+    }
+  }
+  return elm;
+}
+
+let res = concatenatedString('abcs', 'cxzca');
+console.log(res);
