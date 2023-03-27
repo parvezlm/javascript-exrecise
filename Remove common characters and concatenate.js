5b121642364e103f98d1b59cd45eacadf2081b49
parvezlm <parvezstack@gmail.com>
2023-03-27 12:04:17 +0530

    add comments

--- Remove common characters and concatenate.js
+++ Remove common characters and concatenate.js
@@ -1,11 +1,13 @@
 // Remove common characters and concatenate
 
+//concating strings
 function concatenatedString(s1, s2) {
     let elm1 = removeDup(s1, s2);
     let elm2 = removeDup(s2, s1);
     return elm1 + elm2;
 }
 
+// remove duplicates form string
 function removeDup(str1, str2) {
   let elm = '';
   for (let i = 0; i < str2.length; i++) {
