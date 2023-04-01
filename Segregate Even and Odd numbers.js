4a17229671083b23a906d2f309fbcf0f8f02e845
parvezlm <parvezstack@gmail.com>
2023-04-01 12:07:23 +0530

    formate code

--- Segregate Even and Odd numbers.js
+++ Segregate Even and Odd numbers.js
@@ -1,35 +1,35 @@
 // Segregate Even and Odd numbers
 function segregateEvenOdd(arr) {
-  let even = [];
-  let odd = [];
-  for (let elm of arr) {
-    if (elm % 2 === 0) {
-      even.push(elm);
-    } else {
-      odd.push(elm)
+    let even = [];
+    let odd = [];
+    for (let elm of arr) {
+        if (elm % 2 === 0) {
+            even.push(elm);
+        } else {
+            odd.push(elm)
+        }
     }
-  }
-  
-  return [...sortArr(even), ...sortArr(odd)];
-  
+
+    return [...sortArr(even), ...sortArr(odd)];
+
 }
 
 // sort array
 function sortArr(arr) {
-  if (arr.length <= 1) return arr;
-  let pivot = arr[0];
-  let left = [];
-  let right = [];
-  for (let i = 1; i < arr.length; i++) {
-    if (pivot > arr[i]) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
+    if (arr.length <= 1) return arr;
+    let pivot = arr[0];
+    let left = [];
+    let right = [];
+    for (let i = 1; i < arr.length; i++) {
+        if (pivot > arr[i]) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
     }
-  }
-  
-  return [...sortArr(left), pivot, ...sortArr(right)];
-} 
+
+    return [...sortArr(left), pivot, ...sortArr(right)];
+}
 
 let res = segregateEvenOdd([12, 34, 45, 9, 8, 90, 3]);
 console.log(res);
