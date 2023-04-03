528df0e83a9b40d767a146ba638cf93960c2b5fe
parvezlm <parvezstack@gmail.com>
2023-04-03 15:26:09 +0530

    formate code

--- longest substring containing '1'.js
+++ longest substring containing '1'.js
@@ -2,21 +2,21 @@
 function longestStr(str1, str2) {
     let firstStr = findStr(str1)
     let secondStr = findStr(str2);
-    return {firstStr, secondStr};
-  }
-  
-  function findStr(str) {
+    return { firstStr, secondStr };
+}
+
+function findStr(str) {
     let count = 0;
     for (let i = 0; i < str.length; i++) {
-      if (str[i] == 1) {
-        count++;
-      } else {
-        break;
-      }
+        if (str[i] == 1) {
+            count++;
+        } else {
+            break;
+        }
     }
     return count;
-  }
-  
-  let res = longestStr('110', '11101110');
-  console.log(res)
+}
+
+let res = longestStr('110', '11101110');
+console.log(res)
 //   output: 2, 3
