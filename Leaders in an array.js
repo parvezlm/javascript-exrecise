c8af3c008777c8ae238b0a5e66edadcab8f87f7e
parvezlm <parvezstack@gmail.com>
2023-04-04 11:58:34 +0530

    formate code

--- Leaders in an array.js
+++ Leaders in an array.js
@@ -1,31 +1,29 @@
 // Leaders in an array
 function leader(arr, num) {
-  let sum = [];
-  let led = [];
-  for (let i = 0; i < arr.length; i++) {
-    let elm = arr[i];
-    for (let j = i + 1; j < arr.length; j++) {
-      if (elm > arr[j]) {
-         sum.push(true);
-      } else {
-        sum.push(false);
-      }
-    } 
-   
-    if (sum.includes(false)) {
-       sum = [];
-    } else {
-      led.push(arr[i])
+    let sum = [];
+    let led = [];
+    for (let i = 0; i < arr.length; i++) {
+        let elm = arr[i];
+        for (let j = i + 1; j < arr.length; j++) {
+            if (elm > arr[j]) {
+                sum.push(true);
+            } else {
+                sum.push(false);
+            }
+        }
+
+        //empty sum array if any false value is exist in sum array
+        if (sum.includes(false)) {
+            sum = [];
+        } else {
+            led.push(arr[i])
+        }
+
     }
-  }
-  
-  return led;
+    return led;
 }
 
-function checkFalsy(arr) {
-   
-}
 
-let res = leader([16,17,4,3,5,2]);
+let res = leader([16, 17, 4, 3, 5, 2]);
 console.log(res);
 // output: [17, 5, 2]
