dbae9b04f086dac052a46afbe5fa95cf4b834b9b
parvezlm <parvezstack@gmail.com>
2023-05-02 12:36:45 +0530

    formate code

--- reduce()-polyfill.js
+++ reduce()-polyfill.js
@@ -1,16 +1,16 @@
 // reduce() polyfill
 
 Array.prototype.myReduce = function (callback, initial_val) {
- let acc = initial_val;
-  
-  for (let i = 0; i < this.length; i++) {
-     if (acc) {
-       acc = callback(acc, this[i], i, this);
-     } else {
-       acc = this[i];
-     }
-  }
-  return acc;
+    let acc = initial_val;
+
+    for (let i = 0; i < this.length; i++) {
+        if (acc) {
+            acc = callback(acc, this[i], i, this);
+        } else {
+            acc = this[i];
+        }
+    }
+    return acc;
 }
 
 const nums = ['j', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't'];
@@ -18,12 +18,9 @@ let res = nums.myReduce(findNum);
 console.log(res);
 
 function findNum(acc, curr_val, curr_index, arr) {
-  acc += curr_val;
-  return acc;
+    acc += curr_val;
+    return acc;
 }
-
-
-
 
 
 // output: 'javascript'
