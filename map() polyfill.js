d9b3634e415f1c73b2f4f2e4052d7b8288257a16
parvezlm <parvezstack@gmail.com>
2023-05-02 12:36:25 +0530

    format code

--- map() polyfill.js
+++ map() polyfill.js
@@ -1,12 +1,12 @@
 // map() polyfill
 
 let arr = [1, 2, 3, 4, 5];
-Array.prototype.myMap = function(callback) {
-  let arr = [];
-  for (let i = 0; i < this.length; i++) {
-    arr.push(callback(this[i], i, this));
-  }
-  return arr;
+Array.prototype.myMap = function (callback) {
+    let arr = [];
+    for (let i = 0; i < this.length; i++) {
+        arr.push(callback(this[i], i, this));
+    }
+    return arr;
 }
 
 
