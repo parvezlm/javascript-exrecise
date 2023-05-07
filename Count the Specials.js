5ecdb88f44b1832b8258531fff4f1063fbbd77f0
parvezlm <parvezstack@gmail.com>
2023-05-07 11:28:24 +0530

    Count the Specials

---
+++ Count the Specials.js
@@ -0,0 +1,29 @@
+// Count the Specials
+
+function countSpecials(arr, k) {
+    let countObj = findCounts(arr, k);
+    let count = 0;
+    for (let item in countObj) {
+        if (countObj[item] >= k) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function findCounts(arr, k) {
+    let num = Math.floor(arr.length / k);
+    let obj = {};
+    for (let item of arr) {
+        if (item in obj) {
+            obj[item] += 1;
+        } else {
+            obj[item] = 1;
+        }
+    }
+    return obj;
+}
+
+
+let res = countSpecials([1, 4, 1, 2, 4], 2);
+console.log(res);
