67a1fa9776cc6ec23021b548a307d4c0b9157799
parvezlm <parvezstack@gmail.com>
2025-05-21 18:48:56 +0530

    update

--- form-validation/flat an array.js
+++ form-validation/flat an array.js
@@ -14,3 +14,28 @@ function flatArr(arr) {
   console.log(res)
 
   //outpuot : [1,2,3,4,5,6,7,8,9,10]
+
+
+  // ============== updated approach ================
+  let nestedArr = [1,2,[3,4,[5,6,[7,8,9,[10]]]]];
+
+  function flattenArr(arr) {
+    let result = [];
+    
+    arr.forEach(function(item) {
+      if (Array.isArray(item)) {
+          result.push(...flattenArr(item))
+      } else {
+        result.push(item);
+      }
+      
+    })
+    
+    return result;
+  }
+
+  let res2 = flattenArr(nestedArr);
+  console.log(res2)
+
+  //output: [1,2,3,4,5,6,7,8,9,10]
+
