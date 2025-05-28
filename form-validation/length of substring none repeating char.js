e21b793d8a4622f0f1e97bbe257060fe78a23441
parvezlm <parvezstack@gmail.com>
2025-05-28 13:34:16 +0530

    update sol2

--- form-validation/length of substring none repeating char.js
+++ form-validation/length of substring none repeating char.js
@@ -23,4 +23,27 @@ function lengthOfLongestSubstring(s) {
  
  let res = lengthOfLongestSubstring("abcabcbb");;
  console.log(res)
-//output: 3 
+//output: 3
+
+
+//solution 2 using sliding windows
+// function lengthOfLongestSubstring(s) {
+//   let set = new Set();
+//   let maxLength = 0;
+//   let left = 0;
+
+//   for (let right = 0; right < s.length; right++) {
+//       while (set.has(s[right])) {
+//           set.delete(s[left]);
+//           left++;
+//       }
+//       set.add(s[right]);
+//       maxLength = Math.max(maxLength, right - left + 1);
+//   }
+
+//   return maxLength;
+// }
+
+// let res2 = lengthOfLongestSubstring("abcabcbb");
+// console.log(res2);  // Output: 3 ("abc")
+
